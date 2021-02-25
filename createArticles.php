61a4581310273b8a19d93ee4043ad9741b0df085
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-25 15:42:33 +0100

    add a h3 + a form with 3 inputs : 2 for the title and content + 1 type submit :arrow_right: createArticles.php

--- createArticles.php
+++ createArticles.php
@@ -9,9 +9,28 @@ include('connection_database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Create Articles</title>
 </head>
 <body>
+    <h3>Création d'article :</h3>
+
+    <form action="#" method="post">
     
+    <div>
+        <p>
+            <label for="title">Titre :</label>
+            <input type="text" id="title" name="title">
+        </p>
+
+        <p>
+            <label for="content">Contenu :</label>
+            <input type="text" id="content" name="content">
+        </p>
+
+        <input type="submit" value="Submit">
+    </div>
+
+    </form>
 </body>
 </html>
