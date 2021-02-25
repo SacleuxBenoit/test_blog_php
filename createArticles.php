86c41765be23f5ddba516c1e1e8a7ec43fbb1d1b
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-25 15:36:36 +0100

    create the file createArticles.php, add include to infos_admin_database.php, connection_database, session_start + doctype in it

---
+++ createArticles.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include('infos_admin_database.php');
+include('connection_database.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Articles</title>
+</head>
+<body>
+    
+</body>
+</html>
