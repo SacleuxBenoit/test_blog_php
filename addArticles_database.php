9dcbecdf5b7ba1d1e270eb42933544607eedd597
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-25 16:06:06 +0100

    add verification if an input is empty or not + add redirection to index.php :arrow_right: addArticles_database.php

--- addArticles_database.php
+++ addArticles_database.php
@@ -3,8 +3,11 @@ session_start();
 include('infos_admin_database.php');
 include('connection_database.php');
 
-$create_articles = $bdd->prepare('INSERT INTO billets(titre,contenu) VALUES(:titre,:contenu)');
-$create_articles->bindParam(':titre', $_POST['titleArticle']);
-$create_articles->bindParam(':contenu', $_POST['contentArticle']);
-$create_articles->execute();
+if(!empty($_POST['titleArticle']) && !empty($_POST['contentArticle'])){
+    $create_articles = $bdd->prepare('INSERT INTO billets(titre,contenu) VALUES(:titre,:contenu)');
+    $create_articles->bindParam(':titre', $_POST['titleArticle']);
+    $create_articles->bindParam(':contenu', $_POST['contentArticle']);
+    $create_articles->execute();
+    header('Location: ./index.php');
+}
 ?>
