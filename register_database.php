43136ff5e116e5c7d695cd7c8f734c9f8aee7a7e
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-26 16:05:47 +0100

    verify if the pseudo is in the database :arrow_right: register_database.php

--- register_database.php
+++ register_database.php
@@ -3,7 +3,12 @@ session_start();
 include('infos_admin_database.php');
 include('connection_database.php');
 
-if(!empty($_POST['registerPseudo']) && !empty($_POST['registerPassword'])){
+$find_pseudo_database = $bdd->prepare('SELECT pseudo FROM user WHERE pseudo = :pseudo');
+$find_pseudo_database->bindParam(':pseudo', $_POST['registerPseudo']);
+$find_pseudo_database->execute();
+$find_pseudo = $find_pseudo_database->fetch();
+
+if(!empty($_POST['registerPseudo']) && !empty($_POST['registerPassword']) && $_POST['registerPseudo'] != $find_pseudo['pseudo']){
     $password_hash = password_hash($_POST['registerPassword'], PASSWORD_DEFAULT);
 
     $create_user = $bdd->prepare('INSERT INTO user(pseudo,passwordUser) VALUES(:pseudo,:passwordUser)');
