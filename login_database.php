a9868e00962239f83f10dda302f80b1a4b02d497
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-26 15:54:12 +0100

    add query for find if pseudo && password exist in the database :arrow_rigt: login_database.php

--- login_database.php
+++ login_database.php
@@ -3,4 +3,13 @@ session_start();
 include('infos_admin_database.php');
 include('connection_database.php');
 
+$find_user_database = $bdd->prepare('SELECT pseudo,passwordUser FROM user WHERE pseudo = :pseudo');
+$find_user_database->bindParam(':pseudo', $_POST['loginPseudo']);
+$find_user_database->execute();
+$find_user = $find_user_database->fetch();
+
+if($_POST['loginPseudo'] == $find_user['pseudo'] && password_verify($_POST["loginPassword"],$find_user['passwordUser'])){
+    header('Location: index.php');
+}
+
 ?>
