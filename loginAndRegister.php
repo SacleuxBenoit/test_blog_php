15ea837a43c81d46b984fc49bfedf60bce78af10
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-26 14:17:03 +0100

    create the form for the login :arrow_right: loginAndRegister.php

--- loginAndRegister.php
+++ loginAndRegister.php
@@ -7,6 +7,21 @@
     <title>Login / Register</title>
 </head>
 <body>
-    
+    <form action="#" method="post">
+        <div>
+            <h3>Login</h3>
+            <p>
+                <label for="userName">Pseudo :</label>
+                <input type="text" name="userName" id="userName">
+            </p>
+
+            <p>
+                <label for="password">Mot de passe :</label>
+                <input type="password" name="password" id="password">
+            </p>
+
+            <input type="submit" value="Sumbit">
+        </div>
+    </form>
 </body>
 </html>
