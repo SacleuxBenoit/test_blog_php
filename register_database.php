5f95a217569c91ccb1d3d7fe6c7f15dc09d44457
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-26 14:48:23 +0100

    user can now create an account :arrow_right: register_database.php

--- register_database.php
+++ register_database.php
@@ -2,4 +2,15 @@
 session_start();
 include('infos_admin_database.php');
 include('connection_database.php');
+
+if(!empty($_POST['registerPseudo']) && !empty($_POST['registerPassword'])){
+    $create_user = $bdd->prepare('INSERT INTO user(pseudo,passwordUser) VALUES(:pseudo,:passwordUser)');
+    $create_user->bindParam(':pseudo', $_POST['registerPseudo']);
+    $create_user->bindParam(':passwordUser', $_POST['registerPassword']);
+    $create_user->execute();
+    header('Location: ./loginAndRegister.php');
+}else{
+    header('Location: ./loginAndRegister.php');
+}
+
 ?>
