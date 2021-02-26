4dc0fd97fbfe3d616af96c194500ee026a1ef984
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-02-26 15:20:33 +0100

    hash the password :arrow_right: register_database.php

--- register_database.php
+++ register_database.php
@@ -4,9 +4,11 @@ include('infos_admin_database.php');
 include('connection_database.php');
 
 if(!empty($_POST['registerPseudo']) && !empty($_POST['registerPassword'])){
+    $password_hash = password_hash($_POST['registerPassword'], PASSWORD_DEFAULT);
+
     $create_user = $bdd->prepare('INSERT INTO user(pseudo,passwordUser) VALUES(:pseudo,:passwordUser)');
     $create_user->bindParam(':pseudo', $_POST['registerPseudo']);
-    $create_user->bindParam(':passwordUser', $_POST['registerPassword']);
+    $create_user->bindParam(':passwordUser', $password_hash);
     $create_user->execute();
     header('Location: ./loginAndRegister.php');
 }else{
