56697560745924f1bdd59f1da41805349c5fe8d8
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-02 16:07:24 +0100

    add the pseudo to  :arrow_right: database/login_database.php

--- database/login_database.php
+++ database/login_database.php
@@ -3,6 +3,8 @@ session_start();
 include('../infos_admin_database.php');
 include('connection_database.php');
 
+$_SESSION['loginPseudo'] = $_POST['loginPseudo'];
+
 $find_user_database = $bdd->prepare('SELECT pseudo,passwordUser FROM user WHERE pseudo = :pseudo');
 $find_user_database->bindParam(':pseudo', $_POST['loginPseudo']);
 $find_user_database->execute();
