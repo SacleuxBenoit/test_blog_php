22d3da536b71de85653b99e1157d819bad678183
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-03 11:42:43 +0100

    get the id of user in  :arrow_right: login_database.php

--- database/login_database.php
+++ database/login_database.php
@@ -3,12 +3,14 @@ session_start();
 include('../infos_admin_database.php');
 include('connection_database.php');
 
-$_SESSION['loginPseudo'] = $_POST['loginPseudo'];
-
-$find_user_database = $bdd->prepare('SELECT pseudo,passwordUser FROM user WHERE pseudo = :pseudo');
+$find_user_database = $bdd->prepare('SELECT id,pseudo,passwordUser FROM user WHERE pseudo = :pseudo');
 $find_user_database->bindParam(':pseudo', $_POST['loginPseudo']);
 $find_user_database->execute();
 $find_user = $find_user_database->fetch();
+
+// $_SESSION USER
+$_SESSION['id_user'] = $find_user['id'];
+$_SESSION['loginPseudo'] = $_POST['loginPseudo'];
 
 if($_POST['loginPseudo'] == $find_user['pseudo'] && password_verify($_POST["loginPassword"],$find_user['passwordUser'])){
     header('Location: ../index.php');
